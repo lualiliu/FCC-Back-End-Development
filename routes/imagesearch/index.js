3d19f94b1ec2ad3b74f0b6d0dd3db1b6b857f082
lualiliu <lualiliu@outlook.com>
2018-03-10 15:56:59 +0800

    imagesearch

--- routes/imagesearch/index.js
+++ routes/imagesearch/index.js
@@ -3,6 +3,22 @@ var request = require('request');
 var url = require('url');
 var router = express.Router();
 
+router.get('/', function(req, res) {
+  res.sendFile( __dirname + "/" + "index.html" );
+});
+
+router.get('/lastest', function(req, res) {
+	url = "https://cryptic-ridge-9197.herokuapp.com/api/latest/imagesearch/";
+	console.log(url);
+	request(url, function (error, response, body) {
+	if (!error && response.statusCode == 200) {
+	    var jsonArr = JSON.parse(body);
+	    console.log(jsonArr);
+	    res.json(jsonArr);
+	  }
+	})
+	
+});
 
 router.get('/:query', function(req, res) {
 	url = "https://cryptic-ridge-9197.herokuapp.com/api/imagesearch/"+req.params.query;
@@ -11,9 +27,12 @@ router.get('/:query', function(req, res) {
 	if (!error && response.statusCode == 200) {
 	    var jsonArr = JSON.parse(body);
 	    console.log(jsonArr);
+	    res.json(jsonArr);
 	  }
 	})
-	res.json(jsonArr);
+	
 });
+
+
 
 module.exports = router;
