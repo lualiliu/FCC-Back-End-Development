6865bfb0808c30419ee92c7ca756d2b0779072c0
lualiliu <lualiliu@outlook.com>
2018-03-10 16:53:11 +0800

    filemetadata

--- routes/filemetadata/index.js
+++ routes/filemetadata/index.js
@@ -5,21 +5,20 @@ var path   = require("path");
 var upload = multer({dest:(path.join(__dirname,"/uploads"))});         // file destination
 var router = express.Router();
 
-var undone = "<p>Need to upload <a href='https://mgjean-meta.glitch.me/'>back</a><p>"   //redirect home-page 
+var undone = "<p>Need to upload<p>"   //redirect home-page 
 
 router.get("/",function(req,res){
 	res.sendFile( __dirname + "/" + "index.html" );                                  // render homepage
 });
-router.post("/upload",upload.single("image"),function(req,res){
-	if (req.file===undefined){
-		res.send(undone);                                              // if file did not select
-	}
-	res.json({name:req.file.originalname,size: req.file.size});        // return json 
-	fs.unlink(req.file.path,function(err){						       // delete uploaded file
-		if (err){return console.log(err)}
-		console.log("Removed!")});	
-});
-  
+router.post("/upload",upload.single('user-file'),function(req,res){
+    //console.log(req.file);
+    res.writeHead(200, {'content-type':'application/json'});
+    var op = {
+        'Name':req.file.originalname,
+        'Size':req.file.size
+    };
+    res.end(JSON.stringify(op));
+});  
 router.get("/upload",function(req,res){                                   // catch get call to "/upload"
 	res.send(undone);                                                  // redirect home-page
 });
